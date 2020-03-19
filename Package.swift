fb509120611f717f049c0b4651b3afeabe3b1868
Alex Young <ayoung@onewaypath.com>
2020-03-19 11:55:52 -0400

    switch to unixTools

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "test",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/developerinsider/SPMDeveloperInsider.git", from: "1.0.0"),
+        .package(url: "https://github.com/rahyoung/unixTools.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
